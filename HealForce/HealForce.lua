4c02988371d2015e258c8dc337dfe7480826f93f
elitegoliath <elitegoliath13@gmail.com>
2019-03-09 23:05:43 -0600

    Made a specific drag handler for the group frame.

--- HealForce/HealForce.lua
+++ HealForce/HealForce.lua
@@ -1,8 +1,18 @@
 local _, hf = ...;
 
+print('HealForce.lua loaded.')
+
 hf.isLocked = false;
 
-local TestFrame = CreateFrame('Frame', 'Test_Frame', UIParent, 'ArchitypeFrame');
+local TestFrame = CreateFrame('Frame', 'Test_Frame', UIParent, 'HF_GroupFrame');
 
 TestFrame:SetSize(300, 300);
 TestFrame:SetPoint('CENTER', UIParent, 'CENTER');
+
+function StartDrag()
+    TestFrame:StartMoving();
+end;
+
+function StopDrag()
+    TestFrame:StopMovingOrSizing();
+end;
