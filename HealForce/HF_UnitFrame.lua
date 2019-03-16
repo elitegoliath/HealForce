17b2d2ad8e0e577cd0a6abfc616dc3c3a9b0f626
elitegoliath <elitegoliath13@gmail.com>
2019-03-15 20:13:37 -0500

    Got the ability to click the health bar and cast a spell

--- HealForce/HF_UnitFrame.lua
+++ HealForce/HF_UnitFrame.lua
@@ -14,8 +14,8 @@ HF_Unit.__index = HF_Unit;
 local function HF_UpdateHealth(self)
     self.maxHealth = UnitHealthMax(self.name);
     self.currentHealth = UnitHealth(self.name);
-    self.frame:SetMinMaxValues(math.min(0, self.currentHealth), self.maxHealth);
-    self.frame:SetValue(self.currentHealth);
+    self.frame.healthBar:SetMinMaxValues(math.min(0, self.currentHealth), self.maxHealth);
+    self.frame.healthBar:SetValue(self.currentHealth);
 end;
 
 -- Constructor for the HF_Unit class.
@@ -23,8 +23,12 @@ function HF_Unit.new(unitName, parentFrame)
     local self = setmetatable({}, HF_Unit);
 
     -- Generate the unit frame.
-    self.frame = CreateFrame('StatusBar', unitName .. 'UnitFrame', parentFrame, 'HF_UnitFrame');
+    self.frame = CreateFrame('Button', unitName .. 'UnitFrame', parentFrame, 'HF_UnitFrame');
     self.frame.unitName = unitName;
+
+    -- Set initial spell.
+    self.frame:SetAttribute('type', 'spell');
+    self.frame:SetAttribute('spell', 'Flash of Light');
 
     -- Set initial stats.
     self.name = unitName;
@@ -32,8 +36,8 @@ function HF_Unit.new(unitName, parentFrame)
     self.currentHealth = UnitHealth(unitName);
 
     -- Set the frame to match.
-    self.frame:SetMinMaxValues(math.min(0, self.currentHealth), self.maxHealth);
-    self.frame:SetValue(self.currentHealth);
+    self.frame.HealthBar:SetMinMaxValues(math.min(0, self.currentHealth), self.maxHealth);
+    self.frame.HealthBar:SetValue(self.currentHealth);
 
     -- Set class level functions.
     self.UpdateHealth = HF_UpdateHealth;
--- HealForce/HealForce.lua
+++ HealForce/HealForce.lua
@@ -23,19 +23,25 @@ end;
 
 -- All of the event actions are registered here.
 local function HF_EventActions(self, event, ...)
+    local arg1 = select(1, ...);
+
     -- When a player's health or maximum health is changed...
     if (event == 'UNIT_HEALTH') or (event == 'UNIT_MAXHEALTH') then
-        local unitName = select(1, ...);
+        local unitName = arg1;
         local unit = HF_GetUnit('healer', unitName);
 
         if unit then
             unit:UpdateHealth();
         end;
+    elseif (event == 'ADDON_LOADED') and (arg1 == 'HealForce') then
+        -- Create the initial group frame containing the player.
+        HF_GroupCollection['healer'] = HF_Group.new('myGroupFrame', {'player'});
     end;
 end;
 
 -- Register the events we care about in this mod.
 local function HF_RegisterEvents(frame)
+    frame:RegisterEvent('ADDON_LOADED');
     frame:RegisterEvent('UNIT_HEALTH');
     frame:RegisterEvent('UNIT_MAXHEALTH');
     frame:SetScript('OnEvent', HF_EventActions);
@@ -44,7 +50,4 @@ end;
 -- Initialize the mod. Create the first frame, and establish the event listeners.
 function HF_HealForce_Initialize(frame)
     HF_RegisterEvents(frame);
-
-    -- Create the initial group frame containing the player.
-    HF_GroupCollection['healer'] = HF_Group.new('myGroupFrame', {'player'});
 end;
